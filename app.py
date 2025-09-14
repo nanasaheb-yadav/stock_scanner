ae8905e2d758ac705ac79d494dc1f7b00406760b
Nanasaheb Yadav <nanayadav33@gmail.com>
2025-09-14 20:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1339,4 +1339,4 @@ def homepage():
 if __name__ == "__main__":
     import os, uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("complete_scanner_with_fundamentals:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info")
